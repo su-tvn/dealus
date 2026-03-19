540d7b19d501c5406eabcbc5b4cd2b2d5131367a
Thanh <thanh@vastbit.com>
2026-03-19 09:49:56 +0700

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,55 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon.svg'],
+      manifest: {
+        name: 'DealUs - Marketplace',
+        short_name: 'DealUs',
+        description: 'Mạng lưới giao dịch & tư vấn chuyên gia',
+        theme_color: '#1d4ed8',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any'
+          },
+          {
+            src: 'icon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/i\.pravatar\.cc\/.*/i,
+            handler: 'CacheFirst',
+            options: { cacheName: 'avatars-cache', expiration: { maxEntries: 20 } }
+          },
+          {
+            urlPattern: /^https:\/\/images\.unsplash\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: { cacheName: 'images-cache', expiration: { maxEntries: 30 } }
+          }
+        ]
+      }
+    })
+  ],
   build: {
     outDir: 'dist'
   }
